7c8014286bed7d74cc709ba1c305cb1025d7f7e6
MikeoPerfect <hfuboys@outlook.com>
2021-01-10 23:23:58 +0800

    $date init github bingpic action

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def main():
     json_data = json.loads(api.text)
     pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
     start_date = json_data['images'][0]['startdate']
+
     open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
     print('save json file Success!')
     copy_right = json_data['images'][0]['copyright']
+    name = copyright.split("，")[0]
     pic = get(pic_url, stream=True)
     if(pic.status_code == 200):
-        open(r'./picture/{0}.png'.format(copy_right), 'wb').write(pic.content)
+        open(r'./picture/{0}.png'.format(name), 'wb').write(pic.content)
         print('Create Image Success!')
     else:
         print('Create Image Faild!')
