d75a0fbb70fbd8c557e2a1829b5786179bfd3c52
MikeoPerfect <hfuboys@outlook.com>
2021-01-10 22:59:37 +0800

    `date` init github bingpic action

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+from requests import get
+
+def main():
+    api_url = r'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+    api = get(api_url)
+    json_data = json.loads(api.text)
+    pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
+    start_date = json_data['images'][0]['startdate']
+    open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
+    print('save json file Success!')
+    copy_right = json_data['images'][0]['copyright']
+    pic = get(pic_url, stream=True)
+    if(pic.status_code == 200):
+        open(r'./pic/{0}.png'.format(copy_right), 'wb').write(pic.content)
+        print('Create Image Success!')
+    else:
+        print('Create Image Faild!')
+
+if __name__ == "__main__":
+    main()
